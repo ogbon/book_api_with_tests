f34b81de2669ce3e85c66cb3423559681ed7fb0d
ogbon <onethinneedful@gmail.com>
2021-01-19 10:19:05 +0100

    first commit

---
+++ api/server/src/config/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config(); 
+
+
+module.exports = {
+
+  // If using onine database
+  // development: {
+  //   use_env_variable: 'DATABASE_URL'
+  // },
+
+  development: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+
+  test: {
+    database: process.env.DB_NAME_TEST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
